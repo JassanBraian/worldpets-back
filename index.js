a3ad182f3d17933b482f33f7a093342175aa77f7
Alexis <vale.alexis18@gmail.com>
2022-07-06 12:58:52 -0300

    quitado de duplicidad en index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const cors = require('cors');
+
 // Routes
 const publicationRoutes = require('./routes/publicationRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
 app.use(express.json({ limit: '10kb' }));
 
 app.use(cors());
